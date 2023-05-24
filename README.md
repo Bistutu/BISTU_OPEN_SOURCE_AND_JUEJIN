5c5fb3bfc0162a63e921b16070500e898c2222ff
ThinkStu <1914493943@qq.com>
2023-05-24 11:55:44 +0800

    START

---
+++ README.md
@@ -0,0 +1,29 @@
+# 掘金助力高校开源-抽奖程序
+
+纯前端原生 HTML/CSS/JavaScript 编写
+
+<img src="https://oss.thinkstu.com/typora/202305241150808.png?x-oss-process=style/optimize" alt="image-20230524115059718" style="zoom: 20%;" />
+
+程序目录：
+
+```shell
+
+├── index.html
+├── script.js
+└── style.css
+├── images
+│   ├── 图片.png
+```
+
+## 维护者
+
+[@ThinkStu](https://github.com/Bistutu)
+
+## 如何贡献
+
+非常欢迎你的加入！提一个 Issue 或者提交一个 Pull Request。
+
+
+## 使用许可
+
+[MIT](LICENSE) © Richard Littauer
---
+++ script.js
@@ -0,0 +1,59 @@
+var prizes = [
+    {id: 1, img: 'images/1.png', text: '"好好生活"贴纸', count: 90},
+    {id: 2, img: 'images/2.png', text: '"好好学习"贴纸', count: 90},
+    {id: 3, img: 'images/3.png', text: '掘金码赛克编织包', count: 5},
+    {id: 4, img: 'images/4.png', text: '乐高积木', count: 20},
+    {id: 5, img: 'images/5.png', text: 'YOYO草坪绿植', count: 25},
+    {id: 6, img: 'images/6.png', text: '掘金IP棒球帽', count: 10},
+];
+
+var currentIndex = 0;
+var intervalId = null;
+var isDrawing = false;
+var debounceTimer = null;
+
+function showPrize(index) {
+    document.getElementById('prize-img').src = prizes[index].img;
+    document.getElementById('prize-text').innerText = prizes[index].text;
+}
+
+intervalId = setInterval(function () {
+    currentIndex = (currentIndex + 1) % prizes.length;
+    showPrize(currentIndex);
+}, 100);
+
+document.getElementById('draw-btn').addEventListener('click', function () {
+    if (debounceTimer !== null) {
+        return;  // 如果在防抖期间点击了按钮，不做任何事情
+    }
+    debounceTimer = setTimeout(function () {
+        debounceTimer = null;
+    }, 1000);  // 1秒后解除防抖状态
+
+    if (isDrawing) {
+        intervalId = setInterval(function () {
+            currentIndex = (currentIndex + 1) % prizes.length;
+            showPrize(currentIndex);
+        }, 100);
+        document.getElementById('draw-result').innerText = '';
+        isDrawing = false;
+    } else {
+        clearInterval(intervalId);
+
+        var total = prizes.reduce((total, prize) => total + prize.count, 0);
+        var random = Math.floor(Math.random() * total);
+        var prizeId = prizes.find(prize => {
+            random -= prize.count;
+            return random < 0;
+        }).id;
+
+        var prizeIndex = prizes.findIndex(prize => prize.id === prizeId);
+        if (prizeIndex !== -1) {
+            showPrize(prizeIndex);
+            document.getElementById('draw-result').innerText = '恭喜你中奖了！';
+        } else {
+            document.getElementById('draw-result').innerText = '抱歉，未找到对应奖品';
+        }
+        isDrawing = true;
+    }
+});
